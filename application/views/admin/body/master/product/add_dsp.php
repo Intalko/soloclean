5ebe58ea8c847e581796bd8bc69559c10a0087c1
Julius Cesario <julius@lixus.id>
2021-09-13 10:09:44 +0700

    add soloclean

--- application/views/admin/body/master/product/add_dsp.php
+++ application/views/admin/body/master/product/add_dsp.php
@@ -21,20 +21,6 @@
                 <label for="nama_product">Nama Product</label>
                 <input type="text" class="form-control" id="nama_product" name="nama_produk" placeholder="Nama Product yang diinginkan">
               </div>
-			  
-              <div class="form-group">
-                <input id="promo" type="checkbox" name="featured" value="1"><label for="promo">Featured
-                </label>
-              </div>
-                
-              <div class="form-group">
-                <label for="id_kategori">Kategori</label>
-                <select id="id_kategori" name="id_kategori" class="form-control">
-                  <?php foreach($data as $row) {?>
-                    <option value="<?=$row['id']?>"><?=$row['nama_kategori']?></option>
-                  <?php } ?>
-                </select>
-              </div>
 
               <div class="form-group">
                 <label for="produk_image">Image Produk</label>
@@ -42,30 +28,9 @@
               </div>
 			  
               <div class="form-group">
-                <label for="description">Deskripsi produk</label>
-                <textarea name="penjelasan" id="description" placeholder="Tulis Deskripsi Produk anda" class="form-control"></textarea>
+                <label for="link_produk">Link produk (Tokopedia Or Shopee)</label>
+                <input type="text" class="form-control" id="link_produk" name="link_produk" placeholder="Link Product yang diinginkan">
               </div>
-
-              <!-- SEO TAG -->
-              <hr />
-              <h4> SEO TAG (For Google Web Crawler) </h4>
-                <div class="form-group">
-                  <label for="seo_title">SEO Title</label>
-                  <textarea maxlength="150" id="seo_title" name="seo_title" class="form-control"></textarea>
-                </div>
-                <div class="form-group">
-                  <label for="seo_keywords">SEO Keywords</label>
-                  <textarea maxlength="150" id="seo_keywords" name="seo_keywords" class="form-control"></textarea>
-                </div>
-                <div class="form-group">
-                  <label for="seo_description">SEO Description</label>
-                  <textarea maxlength="150" id="seo_description" name="seo_description" class="form-control"></textarea>
-                </div>
-                <div class="form-group">
-                  <label for="seo_author">SEO Authors</label>
-                  <textarea maxlength="150" id="seo_author" name="seo_author" class="form-control"></textarea>
-                </div>
-              <!-- END OF SEO TAG -->
 
             </div><!-- /.box-body -->
             <div class="box-footer">
